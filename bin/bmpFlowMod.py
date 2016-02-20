a1d4c0708803ec854c7413930ea68653890d54dc
Chris Somerlot <csomerlot@brwncald.com>
2016-02-20 13:32:29 -0500

    Init of file, with psuedo-code

---
+++ bin/bmpFlowMod.py
@@ -0,0 +1,13 @@
+
+# import library packages
+import arcpy, os, sys, numpy
+
+# get parameters (input and output datasets, filenames, etc)
+
+# set environment 
+
+# create variables to hold input and output datasets
+
+# process (loop through) datasets
+
+# save outputs
